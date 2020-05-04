354e8dd78c37aefd753708f04e28bc40de892752
Waldemar Porscha <waldemar.porscha@sap.com>
2020-05-04 13:02:49 +0200

    Test with initial PA 15.15.255

--- src/knx/device_object.h
+++ src/knx/device_object.h
@@ -45,7 +45,7 @@ private:
     uint8_t _deviceControl = 0;
     uint8_t _routingCount = 0;
     uint8_t _prgMode = 0;
-    uint16_t _ownAddress = 0;
+    uint16_t _ownAddress = 0xFFFF;
     uint16_t _manufacturerId = 0xfa; //Default to KNXA
     uint32_t _bauNumber = 0;
     char _orderNumber[10];
